0325dcf3c4ef098050bee7dec6e7d65429ad7f1d
Rutesh <ruteshr@mkcl.org>
2022-11-01 17:07:26 +0530

    Added Methods for API for retriving data for single or all courses

--- buildAPI/main.go
+++ buildAPI/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -34,4 +35,28 @@ func (c *Course) IsEmpty() bool {
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Inside Serve Home")
 	w.Write([]byte("<h1> Welcome to API Route</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("All Courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(coursesDB)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Single Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from incoming request
+	params := mux.Vars()
+	fmt.Println(params)
+
+	for _, course := range coursesDB {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course Found for id: ", params["id"])
+	return
 }

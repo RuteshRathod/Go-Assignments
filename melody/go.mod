6eff7f12a9e3db359e9a415e7ec5602ed8aa3d8a
Rutesh <ruteshr@mkcl.org>
2022-12-12 10:40:08 +0530

    Added screenshot and melody POC

---
+++ melody/go.mod
@@ -0,0 +1,7 @@
+module melodyFramework
+
+go 1.18
+
+require github.com/olahol/melody v1.1.1
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ melody/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/olahol/melody"
+)
+
+func main() {
+	m := melody.New()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		m.HandleRequest(w, r)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	http.ListenAndServe(":5000", nil)
+
+}
